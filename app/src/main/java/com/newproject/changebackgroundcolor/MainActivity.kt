a6fffa8a6c007394009c4e4fdecfec1212802f11
bryanladion <ladionbryan29@gmail.com>
2021-10-07 18:24:20 +0800

    changing colors when yellow button click

--- app/src/main/java/com/newproject/changebackgroundcolor/MainActivity.kt
+++ app/src/main/java/com/newproject/changebackgroundcolor/MainActivity.kt
@@ -2,11 +2,25 @@ package com.newproject.changebackgroundcolor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val click_button = findViewById(R.id.click_me_btn) as Button
+        click_button.setOnClickListener{
+            Toast.makeText(applicationContext,"Bryan B. Ladion",Toast.LENGTH_LONG).show()
+        }
+        //yellow
+        var layout = findViewById(R.id.constraintLayout) as ConstraintLayout
+        val yellow =findViewById(R.id.yellow_btn) as Button
+        yellow.setOnClickListener{
+           layout.setBackgroundResource(R.drawable.yellow_background)
+        }
     }
 }
 
